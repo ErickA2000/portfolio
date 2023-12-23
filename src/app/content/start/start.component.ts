d1fa0c23a1fa330d1c0dcaf5f8e46febea2484d0
ErickA2000 <erickwladimirarevalonavarro@gmail.com>
2023-12-23 11:16:05 -0500

    correciones divider en inicio

--- src/app/content/start/start.component.ts
+++ src/app/content/start/start.component.ts
@@ -5,13 +5,16 @@ import { Start } from '../../interfaces/data.interface';
 import { SpinnerComponent } from '../../components/spinner/spinner.component';
 import { deleteSpaceStr } from '../../helper/deleteSpaceStr';
 import { MatDividerModule } from '@angular/material/divider';
+import { NgStyle } from '@angular/common';
+import { DocumentService } from '../../services/document.service';
 
 @Component({
   selector: 'app-start',
   standalone: true,
   imports: [
     SpinnerComponent,
-    MatDividerModule
+    MatDividerModule,
+    NgStyle
   ],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
@@ -19,13 +22,24 @@ import { MatDividerModule } from '@angular/material/divider';
 export class StartComponent implements OnInit {
 
   dataStart?: Start;
+  dividerStyle = {
+    'border-right-color': 'white'
+  }
 
-  constructor( private languageService: LanguageService, private dataService: DataService ){  }
+  constructor( private languageService: LanguageService, private dataService: DataService, private documentService: DocumentService ){  }
 
   ngOnInit(): void {
     this.languageService.language.subscribe( lang => {
       this.dataStart = this.dataService.getStart(lang);
-    })
+    });
+
+    this.documentService.isDarkTheme.subscribe( isDark => {
+      if( isDark ){
+        this.dividerStyle['border-right-color'] = 'white';
+      }else{
+        this.dividerStyle['border-right-color'] = 'black';
+      }
+    });
   }
 
   deleteSpace( value: string | undefined ){
